fe63ab7bc74f7609c84697baa878d221726d562f
James Trenda <jamestrenda@gmail.com>
2025-03-04 16:54:17 -0500

    fix: remove page schema from singletons list

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,6 @@
 import page from './page'
 import homeSettings from './homeSettings'
 
-export const SINGLETONS = [homeSettings.name, page.name]
+export const SINGLETONS = [homeSettings.name]
 
 export const schemaTypes = [page, homeSettings]
